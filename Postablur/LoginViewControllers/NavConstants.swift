f90d5117f2a7942638d1661819214508a987f0b5
YogiMobiware <yogi@mobiwareinc.com>
2018-01-17 15:30:18 +0530

    initial commit

---
+++ Postablur/LoginViewControllers/NavConstants.swift
@@ -0,0 +1,16 @@
+//
+//  NavConstants.swift
+//  Postablur
+//
+//  Created by Srinivas Peddinti on 1/17/18.
+//  Copyright © 2018 MobiwareInc. All rights reserved.
+//
+
+import Foundation
+
+enum NibNamed: String
+{
+   
+    case LoginOptionsVC
+    
+}
